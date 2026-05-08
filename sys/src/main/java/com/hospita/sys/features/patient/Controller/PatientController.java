8f156be5262ac0b3462794f2e3d90d830458180e
Melo <158046523+Mel0o275@users.noreply.github.com>
2026-05-09 01:56:36 +0200

    Donnnnne

--- sys/src/main/java/com/hospita/sys/features/patient/Controller/PatientController.java
+++ sys/src/main/java/com/hospita/sys/features/patient/Controller/PatientController.java
@@ -34,37 +34,37 @@ public class PatientController {
     private AppointmentService appointmentService;
 
     @GetMapping
-    @RequiresPermission("GET_DOCTORS")
+    // @RequiresPermission("GET_DOCTORS")
     public ResponseEntity<ApiResponse> getDoctors() {
         return doctorService.getDoctors();
     }
 
     @GetMapping("/doctors/{id}")
-    @RequiresPermission("GET_DOCTOR")
+    // @RequiresPermission("GET_DOCTOR")
     public ResponseEntity<ApiResponse> getDoctor(@PathVariable Long id) {
         return doctorService.getDoctor(id);
     }
 
     @GetMapping("/history/{id}")
-    @RequiresPermission("GET_HISTORY")
+    // @RequiresPermission("GET_HISTORY")
     public ResponseEntity<ApiResponse> getHistory(@PathVariable long id) {
         return historyService.getHistory(id);
     }
 
     @GetMapping("/appointments/{id}")
-    @RequiresPermission("GET_MY_APPOINTMENTS")
+    // @RequiresPermission("GET_MY_APPOINTMENTS")
     public ResponseEntity<ApiResponse> getMyAppointments(@PathVariable long id) {
         return appointmentService.getMyAppointments(id);
     }
 
     @PostMapping("/appointment")
-    @RequiresPermission("MAKE_APPOINTMENT")
+    // @RequiresPermission("MAKE_APPOINTMENT")
     public ResponseEntity<ApiResponse> makeAppointment(@Valid @RequestBody Appointment appointment) {
         return appointmentService.makeAppointment(appointment);
     }
 
     @PostMapping("/cancel-appointment")
-    @RequiresPermission("CANCEL_APPOINTMENT")
+    // @RequiresPermission("CANCEL_APPOINTMENT")
     public ResponseEntity<ApiResponse> cancelAppointment(@RequestBody CancelAppointmentDto cancelAppointmentDto) {
         return appointmentService.cancelAppointment(cancelAppointmentDto.getSlotid(), cancelAppointmentDto.getPatientid());
     }
@@ -75,7 +75,7 @@ public class PatientController {
     // }
 
     @PostMapping("/make-history")
-    @RequiresPermission("MAKE_HISTORY")
+    // @RequiresPermission("MAKE_HISTORY")
     public ResponseEntity<ApiResponse> makeHistory(@RequestBody historyD historyD) {
         return historyService.makeHistory(historyD.getHistory(), historyD.getId());
     }
